042b20da26e406ab2c8f6b1e6f273c180e51fc03
Celine Rollet <celinerollet12@gmail.com>
2020-03-22 16:33:15 +0100

    js: fonction dans bulle commentaire

--- assets/js/codevores.js
+++ assets/js/codevores.js
@@ -1,1 +1,8 @@
 
+function apparaitreCommentaire(){
+  document.querySelector('.rejoignez-text-bloc').classList.toggle('rejoignez-text-bloc')
+}
+
+document.querySelector('.rejoignez-text-bloc').addEventListener('mouseover', function() {
+  document.querySelector('.commentaire').classList.toggle('rejoignez-text-bloc');
+});
